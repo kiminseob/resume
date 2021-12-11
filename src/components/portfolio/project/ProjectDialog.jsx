a538454f77dc635f35efcc8e7e85f30886781625
kiminseob <kis6473@naver.com>
2021-12-11 16:53:56 +0900

    feat: Project Dialog 내용 추가

--- src/components/portfolio/project/ProjectDialog.jsx
+++ src/components/portfolio/project/ProjectDialog.jsx
@@ -6,6 +6,10 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import Card from '@mui/material/Card';
+import { Box } from '@mui/system';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -17,16 +21,83 @@ export default function ProjectDialog(props) {
   return (
     <div>
       <Dialog
-        fullScreen
         open={open}
         onClose={closeDialog}
         TransitionComponent={Transition}
+        scroll="paper"
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+        maxWidth="desktop"
       >
-        <DialogTitle>Use Google location service?</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
+        <DialogTitle id="scroll-dialog-title">Practice: Todo List</DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText id="scroll-dialog-description">
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginBottom: '1rem',
+              }}
+            >
+              <Card
+                sx={{
+                  width: '50%',
+                  height: '50%',
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  image="/images/todolist.gif"
+                  alt="todolist"
+                />
+              </Card>
+            </Box>
+
+            <Typography variant="subtitle1" gutterBottom>
+              This is a simple to-do list website developed after learning react
+              and mobx for the first time. It has the ability to add, remove,
+              and filter to-do lists. Save the data of the todo list created by
+              utilizing localStorage.
+            </Typography>
+            <Typography variant="h6" gutterBottom align="left">
+              feature
+            </Typography>
+            <Typography variant="body1" component="ul" gutterBottom>
+              <li>
+                - Adding todo Items to the todo list by typing them with the
+                keyboard.
+              </li>
+              <li>
+                - Change to complete state by clicking the checkbox of todo list
+                (add completed class to li tag, added checked attribute to input
+                tag).
+              </li>
+              <li>- Delete the element using the x button in the todo list.</li>
+              <li>
+                - Change to input mode when double-clicking todo list (add
+                editing class to li tag) However, if you press esc key while
+                editing is not completed, it returns to view mode without
+                modification.
+              </li>
+              <li>
+                - Show the number of items counted in todo list at the bottom of
+                the list Check the status value of the todo list and show only
+                the items in the status when you click the to-do or completed
+                job
+              </li>
+              <li>
+                - Storing data in localStorage to reflect the TodoItem&apos;s
+                CRUD. Therefore, you should be able to check the saved data even
+                after refreshing.
+              </li>
+            </Typography>
+            <Typography
+              component="a"
+              variant="h6"
+              href="https://kiminseob.github.io/react-todo-list-step1/"
+            >
+              Demo
+            </Typography>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
