c384c67f891a51d9e86f0ddf1a7b12cb0f3a4f44
kiminseob <kis6473@naver.com>
2021-12-22 00:23:55 +0900

    style: Language ToggleButton 스타일 적용

--- src/components/gnb/Language.jsx
+++ src/components/gnb/Language.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useStore } from 'utils';
+import customTheme from 'scss/variable.module.scss';
 
 function Language() {
   const { NavStore } = useStore();
+  const [value, setValue] = useState('한국어');
 
-  const handleClick = (e) => {
-    NavStore.setLanguage(e.target.value);
+  const handleChange = (e, v) => {
+    NavStore.setLanguage(v);
+    setValue(v);
   };
 
   return (
-    <div className="language">
-      <input type="button" value="한국어" onClick={handleClick} />
-      <input type="button" value="English" onClick={handleClick} />
-    </div>
+    <ToggleButtonGroup
+      color="primary"
+      value={value}
+      size="small"
+      exclusive
+      onChange={handleChange}
+      sx={{
+        '&': {
+          paddingTop: '0.188rem',
+          justifyContent: 'flex-end',
+          '.MuiButtonBase-root': {
+            padding: '0',
+            margin: '0 0.4rem',
+            border: 0,
+            lineHeight: '0.8125rem',
+            '&.Mui-selected': {
+              backgroundColor: 'rgba(0,0,0,0)',
+              color: customTheme.navTxtSelectedColor,
+              borderRadius: 0,
+              borderBottom: `solid ${customTheme.navTxtSelectedColor} 1px`,
+            },
+          },
+        },
+      }}
+    >
+      <ToggleButton value="한국어">한국어</ToggleButton>
+      <ToggleButton value="English">English</ToggleButton>
+    </ToggleButtonGroup>
   );
 }
 
