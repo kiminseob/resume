47eba522a82dd4d22bf194238dc7f89afc5b0124
kiminseob <kis6473@naver.com>
2021-12-30 00:00:32 +0900

    feat: Others 수상 이력 message 추가

--- src/components/portfolio/others/index.jsx
+++ src/components/portfolio/others/index.jsx
@@ -1,41 +1,21 @@
 import React from 'react';
+import { useStore } from 'utils';
 
 function Others() {
+  const { GnbStore } = useStore();
+  const { others } = GnbStore.message;
+  const { award } = others;
+
   return (
     <>
-      <h3>수상 이력</h3>
+      <h3>{award.title}</h3>
       <ul>
-        <li>
-          - 2017.11. LG 스마트씽큐 블루투스 센서를 활용한 앱/서비스 개발 공모전
-          장려상
-          <a href="https://canyon-honey-f8b.notion.site/Closet-Manager-2017-11-2017-12-f9d3f493fd1f41c694819878fc2818e7">
-            link
-          </a>
-        </li>
-        <li>
-          - 2018.12. NUGU Play 개발 공모전 1회 최우수상
-          <a href="https://canyon-honey-f8b.notion.site/NUGU-AIR-2018-11-2020-05-bfaf89c465494bf390aa5afde6b97e91">
-            link
-          </a>
-        </li>
-        <li>
-          - 2019.05. NUGU Play 개발 공모전 2회 우수상
-          <a href="https://canyon-honey-f8b.notion.site/LAST-BREAD-2019-02-2019-04-7d1d824f6b6a4b2da4fadb463dffc12b">
-            link
-          </a>
-        </li>
-        <li>
-          - 2019.09 NUGU Play 개발 공모전 3회 우수상
-          <a href="https://canyon-honey-f8b.notion.site/2019-07-2019-09-4151512d23ba4d789c82de99c05b036a">
-            link
-          </a>
-        </li>
-        <li>
-          - 2019.11 빅스비 캡슐 챌린지 공모전 2회 입상
-          <a href="https://canyon-honey-f8b.notion.site/2019-09-2019-10-11c60248222a4ae2b1d4af8dab73f9e3">
-            link
-          </a>
-        </li>
+        {award.list.map(({ name, link }) => (
+          <li>
+            {name}
+            <a href={link}>link</a>
+          </li>
+        ))}
       </ul>
     </>
   );
--- src/messages/kr/index.js
+++ src/messages/kr/index.js
@@ -2,3 +2,4 @@ export { default as nav } from './nav';
 export { default as project } from './project';
 export { default as about } from './about';
 export { default as profile } from './profile';
+export { default as others } from './others';
